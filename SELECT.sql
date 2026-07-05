e5ee8c2075553ca08180b298c7975ca2360db5be
MattId <matt4ny@gmail.com>
2026-07-05 22:24:12 +0300

    Updated sql files

--- SELECT.sql
+++ SELECT.sql
@@ -1,4 +1,27 @@
-SELECT g.name AS genre_name, COUNT(ag.artist_id) AS artist_count
+SELECT title, duration 
+FROM Track 
+ORDER BY duration DESC LIMIT 1;
+
+SELECT title 
+FROM Track WHERE duration >= 210;
+
+SELECT title 
+FROM Collection WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT name 
+FROM Artist WHERE name NOT LIKE '% %';
+
+SELECT title 
+FROM Track 
+WHERE title LIKE 'My %'     
+   OR title LIKE '% my %'   
+   OR title LIKE '% my'     
+   OR title LIKE 'My'       
+   OR title LIKE 'Мой %' 
+   OR title LIKE '% мой %' 
+   OR title LIKE '% мой' OR title LIKE 'Мой';
+
+   SELECT g.name AS genre_name, COUNT(ag.artist_id) AS artist_count
 FROM Genre g
 JOIN ArtistGenre ag ON g.id = ag.genre_id
 GROUP BY g.name; 
